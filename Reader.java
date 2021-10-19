e771c6884b20b40544da44c2a5b51931ea814282
Mike Babb <mike7400@gmail.com>
2021-10-18 21:11:56 -0400

    Merge branch 'java' of github.com:mkbabb/CSC411-csp-solver into java

--- Reader.java
+++ Reader.java
@@ -1,15 +1,12 @@
 import java.io.FileInputStream;
 import java.util.ArrayList;
 
-import CSP.Variable;
-
 public class Reader {
     public static CSP<Variable, ArrayList<Integer>> readFutoshikiInput(String fileName) {
         final ArrayList<Integer> dom = new ArrayList<Integer>(9);
-        for(int i = 1; i <= dom.size(); i++) dom.set(i, i);
-        
+        for (int i = 1; i <= dom.size(); i++) dom.set(i, i);
+
         CSP<Variable, ArrayList<Integer>> fileCSP;
-        
 
         return fileCSP;
     }
