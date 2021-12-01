863b6e2ce925962762874861b2666266f37811e4
Mike Babb <mike7400@gmail.com>
2021-12-01 16:53:06 -0500

    f

--- csp/sudoku.py
+++ csp/sudoku.py
@@ -71,7 +71,7 @@ def create_sudoku_csp(N: int, values: Dict[int, int], max_solutions: int = 1):
 def create_random_board(N: int, difficulty: SudokuDifficulty = SudokuDifficulty.EASY):
     L = N ** 4
 
-    solution_dir = pathlib.Path("data/sudoku_solutions/")
+    solution_dir = pathlib.Path("./data/sudoku_solutions/")
 
     solutions = list(solution_dir.joinpath(f"{N}").glob("*"))
     solution_filepath: pathlib.Path = random.choice(solutions)
