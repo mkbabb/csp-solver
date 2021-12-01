8b5fec3d39ef6957f7408e5aaac33c6dc8e383e2
Mike Babb <mike7400@gmail.com>
2021-12-01 16:05:25 -0500

    funk

--- app/__init__.py
+++ app/__init__.py
@@ -1,15 +1,7 @@
-# from app.api import app
+from app.api import app
 
-# if __name__ == "__main__":
-#     # app.jinja_env.auto_reload = True
-#     # app.config["TEMPLATES_AUTO_RELOAD"] = True
-
-#     app.run()
-
-from flask import Flask
-app = Flask(__name__)
-@app.route("/")
-def hello():
-    return "Hello, I love Digital Ocean!"
 if __name__ == "__main__":
+    # app.jinja_env.auto_reload = True
+    # app.config["TEMPLATES_AUTO_RELOAD"] = True
+
     app.run()
